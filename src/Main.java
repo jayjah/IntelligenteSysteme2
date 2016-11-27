8cb81e834dbddce05d532b0ab45642b6b5e20cfc
jayjah <markus.krebs@stud.fh-luebeck.de>
2016-11-27 04:13:05 +0100

    init

--- src/Main.java
+++ src/Main.java
@@ -29,7 +29,7 @@ public Main()
 ColorCube cube=new ColorCube(0.3);
    
 Appearance app = new Appearance();
-ColoringAttributes ca = new ColoringAttributes(new Color3f(0.2f, 0.3f, 0.8f), ColoringAttributes.ALLOW_COLOR_WRITE);
+ColoringAttributes ca = new ColoringAttributes(new Color3f(0.2f, 0.6f, 0.8f), ColoringAttributes.ALLOW_COLOR_WRITE);
 app.setColoringAttributes(ca);
    
   // transformgroup.addChild(cube);
@@ -38,14 +38,14 @@ app.setColoringAttributes(ca);
 
    universe.getViewingPlatform().setNominalViewingTransform();
    
-  
+   QuadArray qa=new QuadArray(4*800*1000,GeometryArray.COORDINATES);
    TransformGroup main_matrix=new TransformGroup();
-   float size=0.1f;
+   float size=0.02f;
    for(int y=0;y<1000;y++)
 	   for(int x=0;x<800;x++){
 		   //p1
 		   
-		   QuadArray qa=new QuadArray(4,GeometryArray.COORDINATES);
+		   
 		   
 		   float px=(float)(x-400)*size;
 		   float py=(float)(y-500)*size;
@@ -61,15 +61,16 @@ app.setColoringAttributes(ca);
 		   
 		 //p4
 		   coords[3]=new Point3f(px+size,py+size,0);
-		   qa.setCoordinates(0, coords);
+		   qa.setCoordinates(((y)*800+x)*4, coords);
 		   
-		   Shape3D s3d=new Shape3D(qa,app);
-		   s3d.setGeometry(qa);
-		   //s3d.addGeometry(qa);
-		   main_matrix.addChild(s3d);
+		   
+		  
 	   }
    
-   
+   Shape3D s3d=new Shape3D(qa,app);
+   s3d.setGeometry(qa);
+   //s3d.addGeometry(qa);
+   main_matrix.addChild(s3d);
    
    Transform3D mt=new Transform3D();
    mt.rotX((-Math.PI/2)+0.3);
@@ -82,14 +83,14 @@ app.setColoringAttributes(ca);
    float test=0;
    for(;;){
 	   //universe.cleanup();
-	   transform.setRotation(new AxisAngle4f(new Vector3f(0.001f,0.0f,0.0f),test));
+	   transform.setRotation(new AxisAngle4f(new Vector3f(0.0f,0.0001f,0.0f),test));
 	   transformgroup.setTransform(transform);
-	   test+=0.1;
+	   test+=0.01;
 	   
 	   
 	   try {
 		Thread.currentThread();
-		Thread.sleep(100);
+		Thread.sleep(10);
 	} catch (InterruptedException e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
